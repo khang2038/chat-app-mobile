b7c03d5634d9c7a45fc2ffff04841621e56da9cf
khang2038 <trankhang9c2003@gmail.com>
2023-04-22 16:40:35 +0700

    feat: UI Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import { Link } from 'react-router-native';
 import Button from '../../components/common/Button/Button';
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Image
+          source={require('../../assets/images/background/Illustration.png')}
+        />
+        <Text style={styles.text}>
+          Connect easily with {'\n'} your family and friends over countries
+        </Text>
+      </View>
       <Link to={'/Login'}>
         <Button>
-          <Text style={styles.text_login}>Log in</Text>
+          <Text style={styles.text_login}>Start Messaging</Text>
         </Button>
       </Link>
     </View>
@@ -16,11 +23,27 @@ const Home = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    maxWidth: 330,
+    gap: 120,
+  },
+  top: {
+    alignItems: 'center',
+    gap: 60,
+    textAlign: 'center',
+  },
+  text: {
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 30,
+    textAlign: 'center',
+    color: '#0F1828',
+    width: 280
+  },
   text_login: {
     fontSize: 15,
     lineHeight: 26,
     color: '#fff',
-  },
+  }
 });
 export default Home;
